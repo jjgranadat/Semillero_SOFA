df5f8881aff2de64c00d1c63e6e5b564de9f4381
cesara-montoya1 <118940471+cesara-montoya1@users.noreply.github.com>
2023-01-25 10:58:14 -0500

    Update library
    Se añaden DocStrings y se remueve la función de descarga

--- sofa.py
+++ sofa.py
@@ -1,47 +1,54 @@
-MOD_DICT = {0: -3+3j, #0000
-            1: -3+1j, #0001
-            2: -3-3j, #0010
-            3: -3-1j, #0011
-            4: -1+3j, #0100
-            5: -1+1j, #0101
-            6: -1-3j, #0110
-            7: -1-1j, #0111
-            8:  3+3j, #1000
-            9:  3+1j, #1001
-            10: 3-3j, #1010
-            11: 3-1j, #1011
-            12: 1+3j, #1100
-            13: 1+1j, #1101
-            14: 1-3j, #1110
-            15: 1-1j} #1111
+""" Funciones comunes para aplicaciones de modulación y
+demodulación 16QAM
+"""
+import numpy as np
 
-def download_file(link):
-    # Nombre del archivo
-    file = link.split('/')[-1]
-    
-    # Descarga el archivo si no se ha hecho aún
-    !if ! [[ -f "$file" ]]; then wget $link; fi;
-    
-    # Carga los datos
-    data = loadmat(file)
-    
-    return data
+"""
+Diccionario para modular símbolos 16QAM
+"""
+MOD_DICT = {
+    0: -3 + 3j,  # 0000
+    1: -3 + 1j,  # 0001
+    2: -3 - 3j,  # 0010
+    3: -3 - 1j,  # 0011
+    4: -1 + 3j,  # 0100
+    5: -1 + 1j,  # 0101
+    6: -1 - 3j,  # 0110
+    7: -1 - 1j,  # 0111
+    8: 3 + 3j,  # 1000
+    9: 3 + 1j,  # 1001
+    10: 3 - 3j,  # 1010
+    11: 3 - 1j,  # 1011
+    12: 1 + 3j,  # 1100
+    13: 1 + 1j,  # 1101
+    14: 1 - 3j,  # 1110
+    15: 1 - 1j,
+}  # 1111
+
 
 def mod_norm(const, power):
+    """Calcula el coeficiente de escala para normalizar una señal dada
+    una potencia promedio.
+
+    :param const: constelación.
+    :param power: potencia promedio deseada.
+    :return: coeficiente de escala.
+    """
     constPow = np.mean([x**2 for x in np.abs(const)])
-    scale = np.sqrt(power/constPow)
+    scale = np.sqrt(power / constPow)
     return scale
+
 
 def sync_signals(short_signal, long_signal):
     # Se concatena para asegurar de que el array recibido esté contenido dentro
     # del largo.
     longer_signal = np.concatenate((long_signal, long_signal))
-    correlation = np.correlate(short_signal, longer_signal, mode = "full")
+    correlation = np.correlate(short_signal, longer_signal, mode="full")
     index = np.argmax(correlation) + 1
     delay = index - len(short_signal)
     print(f"El retraso es de {delay} posiciones")
 
     sync_signal = np.roll(longer_signal, delay)
-    sync_signal = sync_signal[:len(short_signal)]
+    sync_signal = sync_signal[: len(short_signal)]
 
     return short_signal, sync_signal
